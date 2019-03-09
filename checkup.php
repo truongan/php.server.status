adef6068be137412a6d1fd9727100e48bb47d8b0
truongan <truongan2005@gmail.com>
2019-03-10 01:21:07 +0700

    add mastodon port

--- checkup.php
+++ checkup.php
@@ -6,6 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<style>
+	.icon{
+		max-width: 50px;
+		max-height: 50px;
+	}
+	</style>
 </head>
 <html><div class="container">
 <?php
@@ -64,9 +70,11 @@ Each service can have a name, port and the Unix domain it run on (default to loc
 $services = array();
 
 
-$services[] = array("port" => "80",       "service" => "Apache",                  "ip" => "") ;
+$services[] = array("port" => "80",       "service" => "Web server",                  "ip" => "") ;
 $services[] = array("port" => "21",       "service" => "FTP",                     "ip" => "") ;
 $services[] = array("port" => "3306",     "service" => "MYSQL",                   "ip" => "") ;
+// $services[] = array("port" => "3000",     "service" => "Mastodon web",                   "ip" => "") ;
+// $services[] = array("port" => "4000",     "service" => "Mastodon streaming",                   "ip" => "") ;
 $services[] = array("port" => "22",       "service" => "Open SSH",				"ip" => "") ;
 $services[] = array("port" => "58846",     "service" => "Deluge",             	"ip" => "") ;
 $services[] = array("port" => "8112",     "service" => "Deluge Web",             	"ip" => "") ;
@@ -80,13 +88,14 @@ foreach ($services  as $service) {
 	if($service['ip']==""){
 	   $service['ip'] = "localhost";
 	}
+	$data .= "<tr><td>" . $service['service'] . "</td><td>". $service['port'];
 
 	$fp = @fsockopen($service['ip'], $service['port'], $errno, $errstr, $timeout);
 	if (!$fp) {
-		$data .= "<tr ><td>" . $service['service'] . "</td><td>". $service['port']."</td><td class='table-danger'>Offline </td></tr>";
+		$data .= "</td><td class='table-danger'>Offline </td></tr>";
 	  //fclose($fp);
 	} else {
-		$data .= "<tr><td>" . $service['service'] . "</td><td>". $service['port']."</td><td class='table-success'>Online</td></tr>";
+		$data .= "</td><td class='table-success'>Online</td></tr>";
 		fclose($fp);
 	}
 
