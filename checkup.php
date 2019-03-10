617e12d8b8ed8fcff49629a2d026ed426e65b2e9
truongan <truongan2005@gmail.com>
2019-03-10 13:43:13 +0700

    change theme

--- checkup.php
+++ checkup.php
@@ -5,18 +5,8 @@
 	<meta content="text/html" charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<<<<<<< HEAD
-	<link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
+	<link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css">
 	<?php //<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">?>
-=======
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-	<style>
-	.icon{
-		max-width: 50px;
-		max-height: 50px;
-	}
-	</style>
->>>>>>> master
 </head>
 <html><div class="container">
 <?php
